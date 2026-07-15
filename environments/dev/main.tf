2c553f241a4434da14f1bc4c2dcf8394e5631c56
shalshal3 <Shalinimohan1999@gmail.com>
2026-07-15 21:45:12 +0530

    Update main.tf

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -8,7 +8,7 @@ module "vpc" {
 
 module "ec2" {
   source = "../../modules/ec2"
-  ami_id = "ami-02d23a03f80ba79fc"
+  ami_id = "ami-01edba92f9036f76e"
   instance_type = "t3.micro"
   subnet_id = module.vpc.subnet_id
   ec2_count = 1
